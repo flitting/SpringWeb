1c3ee3a06a5e2d109b74f5ae93b201683b645ad5
Veros <2038552081@qq.com>
2023-11-16 20:54:46 +0800

    parameter putting

--- src/main/java/com/example/demo/control/HelloController.java
+++ src/main/java/com/example/demo/control/HelloController.java
@@ -3,19 +3,27 @@ package com.example.demo.control;
 import com.example.demo.utils.StringUtils;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloController implements EnvironmentAware {
+    private Environment environment;
     @Resource
     StringUtils stringUtils;
 
+    @Value("${person.prefix:Hi}")
+    String prefix;
     @GetMapping("/api/sayHello")
     public String say(String name, String age){
-        return stringUtils.concat(name,age);
+        String department = environment.getProperty("person.department.name","default");
+        return prefix + department + stringUtils.concat(name,age);
+
     }
 
     @PostMapping("/api/sayHello")
@@ -23,4 +31,10 @@ public class HelloController {
         String text = name +" "+ age +" "+ body;
         return "Hello world, " + text;
     }
+
+    @Override
+    public void setEnvironment(Environment environment){
+        this.environment = environment;
+    }
+
 }
