25bde62e0a7e483837f77d3ebd7f603b9e81e790
caipengfei <caipengfei_ecpkn@outlook.com>
2023-11-21 22:25:30 +0800

    add mysql conenctor

--- src/main/java/com/example/demo/mapper/BookMapper.java
+++ src/main/java/com/example/demo/mapper/BookMapper.java
@@ -1,7 +1,9 @@
 package com.example.demo.mapper;
 
 import com.example.demo.domain.Book;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface BookMapper {
 
     int insert(Book book);
