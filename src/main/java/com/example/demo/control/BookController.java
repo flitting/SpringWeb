9d57f273ad8006bba990323ce0103c9100edaec0
caipengfei <caipengfei_ecpkn@outlook.com>
2023-11-23 21:52:16 +0800

    add some mapper methods

--- src/main/java/com/example/demo/control/BookController.java
+++ src/main/java/com/example/demo/control/BookController.java
@@ -5,41 +5,18 @@ import com.example.demo.domain.JsonResp;
 import com.example.demo.mapper.BookMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 public class BookController {
 
     @Autowired
     BookMapper bookMapper;
-
-    @GetMapping("/books")
-    public ModelAndView booksxxx(){
-        ModelAndView mv = new ModelAndView();
-        Book book = new Book();
-        book.setId(1);
-        book.setName("三国演义");
-        book.setAuthor("罗贯中");
-
-        Book book2 = new Book(2, "红楼梦", "曹雪芹");
-
-        List<Book> list = new ArrayList<>();
-        list.add(book);
-        list.add(book2);
-
-        mv.addObject("books", list);
-        mv.setViewName("books");
-        return mv;
-    }
-
-    @PostMapping("/books")
-    @ResponseBody
+    @PostMapping("/books/add")
     public JsonResp<String> insertBook(String name, String author){
         Book book = new Book();
         book.setName(name);
@@ -47,6 +24,31 @@ public class BookController {
 
         bookMapper.insert(book);
 
+        return new JsonResp.Builder<String>()
+                .ok()
+                .message("OK")
+                .build();
+    }
+
+    @GetMapping("/books")
+    public JsonResp<List<Book>> queryBooks(){
+        List<Book> all = bookMapper.findAll();
+        return new JsonResp.Builder<List<Book>>()
+                .ok()
+                .message("OK")
+                .data(all)
+                .build();
+    }
+
+
+    @DeleteMapping("/books/delete")
+    public JsonResp<String> deleteBook(String name){
+        List<Book> books = bookMapper.findByName(name);
+        if(!books.isEmpty()){
+            for(Book book: books){
+                bookMapper.deleteById(book.getId());
+            }
+        }
         return new JsonResp.Builder<String>()
                 .ok()
                 .message("OK")
--- src/main/java/com/example/demo/mapper/BookMapper.java
+++ src/main/java/com/example/demo/mapper/BookMapper.java
@@ -3,8 +3,16 @@ package com.example.demo.mapper;
 import com.example.demo.domain.Book;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface BookMapper {
 
     int insert(Book book);
+
+    List<Book> findAll();
+
+    List<Book> findByName(String name);
+
+    int deleteById(int id);
 }
